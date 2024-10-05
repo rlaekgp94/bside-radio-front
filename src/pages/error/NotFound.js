7fe9f9e1165abe3b125210040c69a40009b3f780
Dany <dany__k@naver.com>
2024-10-06 01:49:33 +0900

    update not found page desc

--- src/pages/error/NotFound.js
+++ src/pages/error/NotFound.js
@@ -10,6 +10,7 @@ export default function NotFound() {
       <div className="error__inner">
         <div className="body">
           <img className="rabbit" src={ImgListNotItem} alt="error rabbit img 에러 이미지" />
+          <p>접근 권한이 없거나,<br />페이지가 존재하지 않습니다</p>
         </div>
         <div className="foot">
           <button onClick={()=> nav("/")}>홈으로 가기</button>
